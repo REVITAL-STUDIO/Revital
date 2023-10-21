8a108f1577dd1e45537771dc3617fe6597675b97
dishon18 <dishon.mmanyi@aol.com>
2023-10-21 09:50:16 -0400

    added menu content

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,26 @@
 "use client"
 
-import Button from '@/app/components/button/Button'
-import Logo from './Logo'
-
+import Logo from './Logo';
+import './navbar.css';
+import Menu from './menu/Menu';
+import {
+    useState
+} from 'react';
 const Navbar = () => {
+    const [isChecked, setIsChecked] = useState(false);
 
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+    };
 
     return (
-        <div className="flex flex-row items-center justify-between  bg-black h-[80px] fixed w-full ">
+        <div className="flex flex-row items-center justify-between  bg-black h-[80px] fixed w-full">
             <div className='ml-2'>
                 <Logo />
             </div>
-            <div className='mr-6'>
-                <Button label='Menu' />
-            </div>
+            <input type="checkbox" id="active" onChange={handleCheckboxChange} />
+            <label htmlFor="active" className="menu-btn">{isChecked ? "Close" : "Menu"}</label>
+            <Menu />
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        <MissionStatement />
+        <Navbar />
       </div>
     </main>
   )
