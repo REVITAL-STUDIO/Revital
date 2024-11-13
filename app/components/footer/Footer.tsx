8935690e90b38314ae071f8bc99bd9fd75b34462
roycejwilliams <roycewilliamsj@gmail.com>
2024-11-12 21:35:47 -0800

    update

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,7 +2,13 @@
 import Link from "next/link";
 import "./footer.css";
 import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+
+interface WordProps {
+  children: string;
+  progress: MotionValue<number>; // progress should be a MotionValue
+  range: [number, number]; // Assuming range is an array with two numeric values
+}
 
 const Footer = () => {
   const container = useRef<HTMLDivElement>(null);
@@ -16,16 +22,24 @@ const Footer = () => {
   const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
   const paragraph =
-    "<p><span>REVITAL</span> crafts immersive digital experiences that redefine how people engage with brands. At the intersection of technology and art, we bring your vision to life, blending creativity with innovation to build seamless, meaningful interactions. Whether it's through sleek design, advanced functionality, or captivating storytelling, we focus on creating digital solutions that not only enhance brand discovery but also foster lasting connections. From ideation to execution, we partner with you to transform your concepts into experiences that resonate and inspire, making your brand stand out in a rapidly evolving digital landscape.</p>";
+    "REVITAL crafts immersive digital experiences that redefine how people engage with brands. At the intersection of technology and art, we bring your vision to life, blending creativity with innovation to build seamless, meaningful interactions. Whether it's through sleek design, advanced functionality, or captivating storytelling, we focus on creating digital solutions that not only enhance brand discovery but also foster lasting connections. From ideation to execution, we partner with you to transform your concepts into experiences that resonate and inspire, making your brand stand out in a rapidly evolving digital landscape.";
 
   const words = paragraph.split(" ");
-
-  words.map((text, index) => {});
 
   return (
     <div className="footer">
       <motion.h1 ref={container} style={{ opacity }}>
-        {paragraph}
+        <>
+          {words.map((text, index) => {
+            const start = index / words.length;
+            const end = start + 1 / words.length;
+            return (
+              <Word key={index} progress={scrollYProgress} range={[start, end]}>
+                {text}
+              </Word>
+            );
+          })}
+        </>
       </motion.h1>
       <div className="footerContact">
         <h1>Reach Out</h1>
@@ -38,3 +52,15 @@ const Footer = () => {
 };
 
 export default Footer;
+
+const Word: React.FC<WordProps> = ({ children, progress, range }) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+
+  return (
+    <span className="word">
+      <span className="shadow">{children}</span>
+
+      <motion.span style={{ opacity: opacity }}>{children}</motion.span>
+    </span>
+  );
+};
